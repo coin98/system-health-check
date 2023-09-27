1e34311d3b31133e32c865fc9a94c1c6ad484b3f
Bob Jr <nguyennt@coin98.finance>
2023-09-27 18:01:05 +0700

    [main] RPC healthcheck: Send error msg to slack

--- rpc/index.js
+++ rpc/index.js
@@ -1,8 +1,11 @@
-
 const request = require('request')
+const noti_bot = require('noti_bot')
+const notifySlack = noti_bot.slack
+const notifyTelegram = noti_bot.telegram
 const RPC = 'https://rpc.tomochain.com'
 
 const getGasPrice = async () => {
+    const errors = []
     try {
       const result = await new Promise((resolve, reject) => {
         request.post(
@@ -29,14 +32,24 @@ const getGasPrice = async () => {
         const res = result.result
         console.log(parseInt(res))
       } else {
-        console.error(`Cannot get gasPrice`)
-        console.error(result.error)
-        process.exit(-1)
+        errors.push(result.error)
       }
     } catch (e) {
-      console.error(`Cannot get gasPrice`)
-      console.error(e)
-      process.exit(-1)
+        errors.push(e.message)
+    }
+
+    if (errors.length > 0) {
+        let msg = `RPC ${RPC} error \n` + errors.join("\n")
+
+        if (process.env.SLACK_HOOK_KEY && process.env.SLACK_CHANNEL) {
+            notifySlack(msg, process.env.SLACK_HOOK_KEY, process.env.SLACK_CHANNEL, process.env.SLACK_BOTNAME ?? 'rpc-healthcheck', process.env.SLACK_BOT_ICON ?? 'c98')
+        }
+        if (process.env.TELEGRAM_CHAT && process.env.TELEGRAM_TOKEN) {
+            notifyTelegram(msg, process.env.TELEGRAM_TOKEN, process.env.TELEGRAM_CHAT, true)
+        }
+        console.error(msg)
+        await sleep(1000)
+        process.exit(-1)
     }
   }
   
